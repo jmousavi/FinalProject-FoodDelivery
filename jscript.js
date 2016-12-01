ef5a64fce60d35e5357d2d9cfbf6d8df3a66b914
jmousavi <jmousavi@mail.csuchico.edu>
2016-11-30 20:54:18 -0800

    skeleton

---
+++ jscript.js
@@ -0,0 +1,36 @@
+myList = [];
+function add(){
+var array = [];
+var price = document.getElementById("food").innerHTML;
+var picture = document.getElementById("food").src;
+price = price.slice(5);
+var myCookie = getCookie("food");
+if (myCookie==null){
+     window.alert("does not exist");
+}
+else{
+	window.alert("exists");
+}
+}
+//courtesy of w3schools at http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//courtesy of w3schools at http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
